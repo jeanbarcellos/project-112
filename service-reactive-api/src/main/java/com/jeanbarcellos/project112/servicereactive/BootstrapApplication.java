37583a69af17e0e7788bfa11f8f8aa76ecb64834
Jean Barcellos <jeanbarcellos@hotmail.com>
2025-04-19 20:30:40 -0300

    Reactive - Ajustes

--- service-reactive-api/src/main/java/com/jeanbarcellos/project112/servicereactive/BootstrapApplication.java
+++ service-reactive-api/src/main/java/com/jeanbarcellos/project112/servicereactive/BootstrapApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Project112Application {
+public class BootstrapApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Project112Application.class, args);
+		SpringApplication.run(BootstrapApplication.class, args);
 	}
 
 }
